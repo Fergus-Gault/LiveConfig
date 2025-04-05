09c39aaacab75d5e8df68784a291d2e69b675c35
Fergus Gault <fergus@gault.com>
2025-04-05 16:57:17 +0100

    Improved basic web interface

--- livevars/interfaces/web/server.py
+++ livevars/interfaces/web/server.py
@@ -1,11 +1,19 @@
-from flask import Flask
+from flask import Flask, render_template
 from livevars.core import manager
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='frontend/templates', static_folder='frontend/static')
 
 @app.route('/')
 def index():
-    return f"{manager.live_functions}"
+    return render_template('index.html')
+
+@app.route('/functions')
+def functions():
+    return render_template('functions.html')
+
+@app.route('/classes')
+def classes():
+    return render_template('classes.html')
 
 
 
